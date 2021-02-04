8abadd7b702da4410a9d4c0282f0d987b84fcc53
SarahKettell <sakettell@gmail.com>
2021-02-04 11:03:56 -0500

    Update: Side Nav design updated

--- src/SideNav.js
+++ src/SideNav.js
@@ -1,36 +1,82 @@
 import React, {useEffect, useState, Component} from 'react';
 import './css/sidenav-styles.css';
 
+function ExpandNav(){
+
+}
+
 export default function SideNav(){
 
     return(
         <aside className="side-nav">
-                <div className="row">
-                    <div className="col">
-                        <div className="list-group" id="list-tab" role="tablist">
-                            <a className="list-group-item list-group-item-action active" id="list-home-list"
-                               data-toggle="list" href="#list-home" role="tab" aria-controls="home">
+            <div className="accordion" id="side-nav-menu">
+                <div className="card">
+                    <div className="card-header" id="syllabusRequirements">
+                        <h5 className="mb-0">
+                            <a className="list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                 <i className="material-icons mdc-list-item__graphic" aria-hidden="true">rule</i>
                                 Syllabus Requirements
+                                <i className="material-icons mdc-list-item__graphic expand-icon" aria-hidden="true"></i>
                             </a>
-                            <a className="list-group-item list-group-item-action" id="list-profile-list"
-                               data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">
-                                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">post_add</i>
-                                Build a Syllabus
-                            </a>
-                            <a className="list-group-item list-group-item-action" id="list-messages-list"
-                               data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">
-                                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">preview</i>
-                                Preview Syllabus
-                            </a>
-                            <a className="list-group-item list-group-item-action" id="list-settings-list"
-                               data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">
-                                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">download_for_offline</i>
-                                Generate Empty Template
-                            </a>
+                        </h5>
+                    </div>
+                    <div id="collapseOne" className="collapse show" aria-labelledby="syllabusRequirements"
+                         data-parent="#syllabusRequirements">
+                        <div className="card-body">
+                            <div className="list-group">
+                                <a href="#" className="list-group-item">Link item</a>
+                                <a href="#" className="list-group-item active">Active link item</a>
+                                <a href="#" className="list-group-item">Link item</a>
+                                <a href="#" className="list-group-item">Link item</a>
+                                <a href="#" className="list-group-item">Link item</a>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className="card">
+                    <div className="card-header" id="buildSyllabus">
+                        <h5 className="mb-0">
+                            <a className="list-group-item list-group-item-action" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">post_add</i>
+                                Build a Syllabus
+                                <i className="material-icons mdc-list-item__graphic expand-icon" aria-hidden="true"></i>
+                            </a>
+                        </h5>
+                    </div>
+                    <div id="collapseTwo" className="collapse show" aria-labelledby="buildSyllabus"
+                         data-parent="#buildSyllabus">
+                        <div className="card-body">
+                            <div className="list-group">
+                                <a href="#" className="list-group-item">Link item</a>
+                                <a href="#" className="list-group-item active">Active link item</a>
+                                <a href="#" className="list-group-item">Link item</a>
+                                <a href="#" className="list-group-item">Link item</a>
+                                <a href="#" className="list-group-item">Link item</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-header" id="previewSyllabus">
+                        <h5 className="mb-0">
+                            <a className="list-group-item list-group-item-action collapsed">
+                                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">preview</i>
+                                Preview Syllabus
+                            </a>
+                        </h5>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-header" id="downloadTemplate">
+                        <h5 className="mb-0">
+                            <a className="list-group-item list-group-item-action collapsed">
+                                <i className="material-icons mdc-list-item__graphic" aria-hidden="true">download_for_offline</i>
+                                Download a Template
+                            </a>
+                        </h5>
+                    </div>
+                </div>
+            </div>
         </aside>
     );
 }
